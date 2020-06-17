e11a647280e09b5273af9749cf614771af10b92c
Craig McLeod <mcleod.craig@outlook.com>
2020-06-17 21:19:12 +0100

    Had a go at the next ones but couldn't get anywhere

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -2,7 +2,27 @@ const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
+PangramFinder.prototype.aToZ = function () {
+  alphabetSorted = this.alphabet.sort();
+  return alphabetSorted.reduce((accumulator, currentLetter) => {
+    return accumulator + currentLetter
+  })
+
+}
+
+PangramFinder.prototype.toArray = function(string){
+  return string.split('');
+}
+
 PangramFinder.prototype.isPangram = function () {
+  alphabet = this.aToZ()
+  let array = this.toArray()
+  const arrayReduced =
+  array.filer(function(letter, i){
+    return alphabet.indexOf(letter) == i;
+  })
+  const arrayToZ = arrayReduced.aToZ()
+  return arrayToZ === this.alphabet.aToZ()
 
 }
 
